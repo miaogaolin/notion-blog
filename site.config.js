93d5eab7e71cd66cbc9b704cd7a42e15d3163b6f
苗高林 <2825873215@qq.com>
2021-12-16 21:29:56 +0800

    Update Github

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: '',
-  github: 'miaogaolin',
+  github: '',
   linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
