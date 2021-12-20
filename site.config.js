9f7b2338123ec3bf601091d6358595ca7fe75119
miaogaolin <57740293+miaogaolin@users.noreply.github.com>
2021-12-20 15:57:18 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -29,7 +29,7 @@ module.exports = {
 
   // image CDN host to proxy all image requests through (optional)
   // NOTE: this requires you to set up an external image proxy
-  imageCDNHost: "https://laomiao.laomiao.workers.dev",
+  imageCDNHost: "https://image.laomiao.workers.dev",
 
   // Utteranc.es comments via GitHub issue comments (optional)
   utterancesGitHubRepo: null,
