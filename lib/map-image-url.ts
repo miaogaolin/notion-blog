cbc37d0db38ff8a6db56c0b169f6f66f3f5e5a3a
DESKTOP-RKMKLHA\owner <2825873215@QQ.COM>
2022-04-27 17:30:34 +0800

    Merge set image size

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -1,12 +1,30 @@
 import { Block } from 'notion-types'
 import { defaultMapImageUrl } from 'react-notion-x'
 
-import { defaultPageIcon, defaultPageCover } from './config'
+import { defaultPageIcon, defaultPageCover, imageWidth } from './config'
 
 export const mapImageUrl = (url: string, block: Block) => {
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url
   }
 
-  return defaultMapImageUrl(url, block)
+  url = defaultMapImageUrl(url, block)
+  // more recent versions of notion don't proxy unsplash images
+  if (url.startsWith('https')) {
+    if (!url.startsWith('https://images.unsplash.com')) {
+      const notionImageUrlV2 = new URL(url)
+      notionImageUrlV2.searchParams.set('width', imageWidth.toString())
+      url = notionImageUrlV2.toString()
+    } else {
+      // optimize image
+      // webp
+      url = url.replace(/fm=(gif|jpeg|png|jpg|bmp)/i, "fm=webp")
+      if (imageWidth && !url.match(/(\?|=)w=\d+/)) {
+        url += "&w=" + imageWidth
+      }
+    }
+  }
+
+  console.log(url)
+  return url
 }
